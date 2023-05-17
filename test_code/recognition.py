4fd60fb683c123884ee493eefb614bdea859de80
Alex Cui <cuizhihui030925@outlook.com>
2023-05-17 22:27:32 +0800

    Update recognition.py

--- test_code/recognition.py
+++ test_code/recognition.py
@@ -1,9 +1,18 @@
 import mediapipe as mp
-from mediapipe.framework.formats import landmark_pb2
 import cv2
 import time
 
+DEBUG = True
+
 MODEL_PATH = './gesture_recognizer.task'
+
+HAND_COLOR = [
+    (0xff, 0xcc, 0x66),
+    (0x1b, 0xb1, 0xff),
+    (20, 20, 20)
+]
+
+FINGER_TIP = [4, 8, 12, 16, 20]
 
 class Timer:
     def __init__(self):
@@ -29,6 +38,12 @@ if __name__ == '__main__':
         height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
         timer = Timer()
+
+        gesture_counter = [0 for _ in range(7)]
+        current_gesture = None
+        stable_gesture = 0
+        last_time = timer.get_ms()
+        fps_last_time = timer.get_ms() #DEBUG
         
         while True:
             is_grabbed, frame = video.read()
@@ -39,15 +54,67 @@ if __name__ == '__main__':
 
             result = recognizer.recognize_for_video(image, timer.get_ms())
             
-            for landmarks in result.hand_landmarks:
-                landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-                landmarks_proto.landmark.extend([landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in landmarks])
-                mp.solutions.drawing_utils.draw_landmarks(
-                    frame, landmarks_proto, mp.solutions.hands.HAND_CONNECTIONS,
-                    mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
-                    mp.solutions.drawing_styles.get_default_hand_connections_style()
-                )
+            hand_count = len(result.handedness)
+            finger_state = [None, None]
+
+            for i in range(hand_count):
+                landmarks = result.hand_landmarks[i]
+                hand_index = result.handedness[i][0].index
+                positions = [(int(landmark.x * width), int(landmark.y * height)) for landmark in landmarks]
+                finger_state[hand_index] = [landmarks[tip].y < landmarks[tip - 2].y for tip in FINGER_TIP[1:]]
+
+                if DEBUG:
+                    for point1, point2 in mp.solutions.hands.HAND_CONNECTIONS:
+                        cv2.line(frame, positions[point1], positions[point2], HAND_COLOR[hand_index], 3)
+                    for position in positions:
+                        cv2.circle(frame, position, 5, HAND_COLOR[hand_index], -1)
             
+            if DEBUG:
+                for i in range(2):
+                    if finger_state[i] == None:
+                        continue
+                    for finger_index in range(4):
+                        point = [(10 + i * 100 + finger_index * 20, 10), (25 + i * 100 + finger_index * 20, 30)]
+                        cv2.rectangle(frame, point[0], point[1], HAND_COLOR[i] if finger_state[i][finger_index] else HAND_COLOR[-1], -1)
+
+            for i in range(2):
+                if finger_state[i] == None:
+                    continue
+                if finger_state[i][0]:
+                    if finger_state[i][1]:
+                        if finger_state[i][2]:
+                            if finger_state[i][3]:
+                                gesture_counter[-1] += 1
+                            else:
+                                gesture_counter[i * 3 + 2] += 1
+                        else:
+                            gesture_counter[i * 3 + 1] += 1
+                    else:
+                        gesture_counter[i * 3 + 0] += 1
+            
+            if timer.get_ms() - last_time > 1500:
+                last_time = timer.get_ms()
+                possible_gesture = 0
+                for i in range(7):
+                    if gesture_counter[i] > gesture_counter[possible_gesture]:
+                        possible_gesture = i
+                if current_gesture == possible_gesture:
+                    stable_gesture += 1
+                else:
+                    stable_gesture = 0
+                current_gesture = possible_gesture if possible_gesture < 6 and gesture_counter[possible_gesture] > 0 else None
+                gesture_counter = [0 for _ in range(7)]
+            
+            if DEBUG:
+                text = str(stable_gesture)
+                text += 'none' if current_gesture == None else '{}{}'.format('right' if current_gesture // 3 == 0 else 'left', current_gesture % 3 + 1)
+                cv2.putText(frame, text, (40, 200), cv2.FONT_HERSHEY_PLAIN, 2, (255, 102, 102), 2)
+
+            if DEBUG:
+                cur_time = timer.get_ms()
+                cv2.putText(frame, '{} fps'.format(round(1000 / (cur_time - fps_last_time), 2)), (200, 40), cv2.FONT_HERSHEY_PLAIN, 2, (255, 102, 102), 2)
+                fps_last_time = cur_time
+
             cv2.imshow('video', frame)
 
             if cv2.waitKey(1) & 0xff == ord('q'):
