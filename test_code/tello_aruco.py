4d9a132c9d3432353f5d33abb146b04ed3ce8c03
EternalLightning <54901910+EternalLightning@users.noreply.github.com>
2023-05-19 14:24:43 +0800

    Update tello_aruco.py

--- test_code/tello_aruco.py
+++ test_code/tello_aruco.py
@@ -5,10 +5,11 @@ from robomaster.robot import Drone
 from robomaster.flight import Flight
 from robomaster.camera import Camera
 import cv2.aruco
+import numpy as np
 
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 aruco_params = cv2.aruco.DetectorParameters()
-times = 3
+times = 2.5
 camera_center = (480, 360)
 config.LOCAL_IP_STR = '192.168.10.3'
 currentCount = 0
@@ -33,7 +34,7 @@ while True:
     topCenter = [0, 0]
     bottomCenter = [0, 0]
 
-    if len(corners) > 0:
+    if len(corners) > 0 and currentCount != 2:
         ids = ids.flatten()
         for (markerCorner, markerID) in zip(corners, ids):
             # TOP-LEFT, TOP-RIGHT, BOTTOM-RIGHT, BOTTOM-LEFT
@@ -41,10 +42,6 @@ while True:
                 continue
             elif currentCount == 1 and markerID != 30:
                 continue
-            elif currentCount == 2:
-                flight.land()
-                drone.close()
-                exit(0)
 
             corners = markerCorner.reshape((4, 2))
             (topLeft, topRight, bottomRight, bottomLeft) = corners
@@ -73,20 +70,66 @@ while True:
             print('bottomCenter: {}, topCenter: {}'.format(bottomCenter[1], topCenter[1]))
             print('cX: {}, cY: {}\nheight: {}\n'.format(cX, cY, bottomCenter[1] - topCenter[1]))
             print("Objective: {} {}".format(cX, cY - times * (bottomCenter[1] - topCenter[1])))
-            print((cX - camera_center[0]) ** 2 + (camera_center[1] - cY + times * (bottomCenter[1] - topCenter[1])) ** 2)
+            print(
+                (cX - camera_center[0]) ** 2 + (camera_center[1] - cY + times * (bottomCenter[1] - topCenter[1])) ** 2)
 
             if (cX - camera_center[0]) ** 2 + (
                     camera_center[1] - cY + times * (bottomCenter[1] - topCenter[1])) ** 2 <= 10000:
-                flight.rc(-5, 20, 0)
+                flight.rc(-5, 35, 0)
                 print('forward')
-                delay(2)
+                time.sleep(5)
                 currentCount = currentCount + 1
+
             else:
-                flight.rc((cX - camera_center[0]) * 40 / 640, 0,
+                flight.rc((cX - camera_center[0]) * 45 / 480, 0,
                           (camera_center[1] - cY + times * (bottomCenter[1] - topCenter[1])) * 30 / 360)
                 # a:y b:z c:x
                 print("calibrate")
             time.sleep(1)
-    # else:
-    #     break
+            break
+    else:
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, np.array([0, 50, 50]),
+                           np.array([10, 255, 255])) + cv2.inRange(hsv, np.array([170, 50, 50]),
+                                                                   np.array([180, 255, 255]))
+
+        # cv2.imshow('color0', mask)
+        # cv2.waitKey()
+
+        circles = cv2.HoughCircles(
+            mask, cv2.HOUGH_GRADIENT, 1, 30,
+            param1 = None, param2 = 30,
+            minRadius = 10, maxRadius = 0
+        )
+        count = 0
+        circle = [0, 0, 0]
+
+        # !!! SHOULD THE FOLLOWING CODE BE IN THE WHILE LOOP???????
+        circles = list(circles)
+        # find the circle with maximum radius
+        circles.sort(key = lambda x: x[0][2], reverse = True)
+        circle += circles[0][0]
+        count += 1
+
+        # !!! WHAT ARE YOU FUCKING DOING
+        # trying to get the average circle
+        if count >= 10:
+            circle = circle / count
+
+        x = int(circle[0])
+        y = int(circle[1])
+        r = int(circle[2])
+        # rgb_pic = cv2.circle(image, (x, y), r, (0, 0, 255), 3)  # 显示圆
+        # rgb_pic = cv2.circle(rgb_pic, (x, y), 2, (255, 255, 0), -1)  # 显示圆心
+        # cv2.imshow('new', rgb_pic)
+        # cv2.waitKey()
+        # cv2.destroyAllWindows()
+        if abs(x - camera_center[0]) <= 100:
+            flight.rc(0, 20, 0)
+            time.sleep(4)
+            flight.rotate(90).wait_for_completed()
+            currentCount = currentCount + 1
+            flight.land()
+        else:
+            flight.rc((x - camera_center[0]) * 45 / 480, 0, 0)
 # flight.land()
