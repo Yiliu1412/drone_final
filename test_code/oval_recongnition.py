3f33b52b7df076b5a66bbd9ec19cb2bcee648787
EternalLightning <54901910+EternalLightning@users.noreply.github.com>
2023-05-19 21:38:21 +0800

    Update oval_recongnition.py

--- test_code/oval_recongnition.py
+++ test_code/oval_recongnition.py
@@ -1,20 +1,24 @@
 import robomaster
+from robomaster import config
 from robomaster.robot import Drone
+from robomaster.flight import Flight
 from robomaster.camera import Camera
 import cv2
 import numpy as np
 
+
+config.LOCAL_IP_STR = '192.168.10.3'
 drone = Drone()
 drone.initialize()
 
 camera: Camera = drone.camera
-
+flight: Flight = drone.flight
 camera.start_video_stream(display=True)
 
 while True:
-    frame = drone.read_video_frame()
+    frame = camera.read_cv2_image(strategy="newest")
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
+
     # 红色
     lower_red = np.array([0, 50, 50])
     upper_red = np.array([10, 255, 255])
@@ -25,7 +29,7 @@ while True:
     mask = mask1 + mask2
     kernel = np.ones((5,5),np.uint8)
     opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    
+
     # 椭圆识别
     contours, _ = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     ellipse = None
@@ -45,39 +49,37 @@ while True:
             angle = ellipse[2]
             if angle > 90:
                 angle -= 180
-                
-                
+
             # 将无人机飞向椭圆中心并旋转
             if center_x < frame.shape[1]//2:
-                drone.rotate_ccw(20)
+                flight.rotate(-20)
             elif center_x > frame.shape[1]//2:
-                drone.rotate_cw(20)
-            if center_y < frame.shape[0]//2:
-                drone.move_up(20)
-            elif center_y > frame.shape[0]//2:
-                drone.move_down(20)
+                flight.rotate(20)
+            # if center_y < frame.shape[0]//2:
+            #     flight.move_up(20)
+            # elif center_y > frame.shape[0]//2:
+            #     flight.move_down(20)
             if abs(angle) > 5:
                 if angle > 0:
-                    drone.rotate_cw(10)
+                    flight.rotate(10)
                 else:
-                    drone.rotate_ccw(10)
+                    flight.rotate(-10)
             else:
                 # 调整无人机位置，使椭圆变成圆形
                 axis_x, axis_y = ellipse[1]
                 if axis_x > axis_y:
-                    drone.move_left(20)
+                    flight.left(10)
                 elif axis_x < axis_y:
-                    drone.move_right(20)
+                    flight.right(10)
                 if axis_x - axis_y > 50:
-                    drone.move_forward(20)
+                    flight.forward(10)
                 elif axis_y - axis_x > 50:
-                    drone.move_back(20)
-                    
+                    flight.backward(10)
+
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
 camera.stop_video_stream()
-drone.close
+drone.close()
 cv2.destroyAllWindows()
