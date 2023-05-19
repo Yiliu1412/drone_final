40365522c14febd752421c15fc53459016bbc7e0
LLAYUAN <110158781+LLAYUAN@users.noreply.github.com>
2023-05-19 09:07:51 +0800

    Update tello_aruco.py

--- test_code/tello_aruco.py
+++ test_code/tello_aruco.py
@@ -1,27 +1,36 @@
 import robomaster
+from robomaster import config
 from robomaster.robot import Drone
 from robomaster.flight import Flight
 from robomaster.camera import Camera
 import cv2
 import cv2.aruco
+import time
 
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
 aruco_params = cv2.aruco.DetectorParameters()
+config.LOCAL_IP_STR='192.168.10.2'
 drone = Drone()
 drone.initialize()
 print(drone.get_drone_version())
 print(drone.get_sn())
 flight: Flight = drone.flight
 camera: Camera = drone.camera
-camera.start_video_stream(display = True)
+#camera.start_video_stream(display = True)
 
 # image = cv2.imread('test2.jpg', cv2.IMREAD_COLOR)
 # height, width = image.shape[:2]
 # size = (1280, 720) # delete when using frame
 # image = cv2.resize(image, size) # too
-flight.takeoff()
+# flight.takeoff()
+
+
+cv2.imwrite('test3.jpg', image)
+exit(0);
 while True:
     image = camera.read_cv2_image(strategy = "newest")
+    cv2.imwrite('../test3.jpg',image)
+    break
     corners, ids, rejected = cv2.aruco.detectMarkers(image, aruco_dict, parameters = aruco_params)
     cX, cY = (0, 0)
     topCenter = [0, 0]
@@ -48,7 +57,7 @@ while True:
             cX = int((topLeft[0] + bottomRight[0]) / 2.0)
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)
             topCenter = ((topLeft[0] + topRight[0]) / 2, (topLeft[1] + topRight[1]) / 2)
-            # bottomCenter = ((bottomRight[0]+bottomLeft[0])/2, (bottomRight[1]+bottomLeft[1])/2)
+            bottomCenter = ((bottomRight[0]+bottomLeft[0])/2, (bottomRight[1]+bottomLeft[1])/2)
             # print(cX, cY)
             # cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
             # cv2.circle(image, (camera_center[0], camera_center[1]), 4, (0, 0, 255), -1)
@@ -57,11 +66,18 @@ while True:
             # print("[INFO] ArUco marker ID: {}".format(markerID))
             # cv2.imshow("Image", image)
             # cv2.waitKey(0)
-    if (cX - camera_center[0]) ** 2 + (camera_center[1] - cY + times * (bottomCenter[1] - topCenter[1])) ** 2 <= 10000:
-        flight.rc(0, 0, 10)
-        break
-    else:
-        flight.rc((camera_center[0] - cX) * 50 / 640,
-                  (camera_center[1] - cY + times * (bottomCenter[1] - topCenter[1])) * 50 / 360, 10)
-        # a:y b:z c:x
-flight.land()
+            print('bottomCenter: {}, topCenter: {}'.format(bottomCenter[1], topCenter[1]))
+            print('cX: {}, cY: {}\nheight: {}\n'.format(cX, cY, bottomCenter[1] - topCenter[1]))
+            print("Objective: {} {}".format(-cX + camera_center[0], camera_center[1] - cY ))
+            print((cX - camera_center[0]) ** 2 + (camera_center[1] - cY - times * (bottomCenter[1] - topCenter[1])) ** 2)
+            time.sleep(1)
+        if (cX - camera_center[0]) ** 2 + (camera_center[1] - cY - times * (bottomCenter[1] - topCenter[1])) ** 2 <= 10000:
+            #flight.rc(0, 0, 10)
+            print('forward')
+
+        else:
+            # flight.rc((camera_center[0] - cX) * 50 / 640,
+            #           (camera_center[1] - cY + times * (bottomCenter[1] - topCenter[1])) * 50 / 360, 10)
+            # a:y b:z c:x
+            print("calibrate")
+# flight.land()
